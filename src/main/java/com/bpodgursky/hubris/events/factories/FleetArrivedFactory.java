85fa181ff912e6d85e9eaa0643887d89b9ab7c02
Chris Mullins <chris.mullins10@gmail.com>
2013-05-27 18:14:21 -0700

    add fleet spotted

--- src/main/java/com/bpodgursky/hubris/events/factories/FleetArrivedFactory.java
+++ src/main/java/com/bpodgursky/hubris/events/factories/FleetArrivedFactory.java
@@ -3,25 +3,26 @@ package com.bpodgursky.hubris.events.factories;
 import com.bpodgursky.hubris.events.FleetArrivedEvent;
 import com.bpodgursky.hubris.universe.Fleet;
 import com.bpodgursky.hubris.universe.GameState;
+import com.google.common.collect.Lists;
 
-import java.util.Collections;
 import java.util.List;
 
 public class FleetArrivedFactory implements EventFactory<FleetArrivedEvent> {
   @Override
   public List<FleetArrivedEvent> getEvents(GameState newState) {
     GameState oldState = newState.previousState();
+    List<FleetArrivedEvent> events = Lists.newArrayList();
 
     for (Fleet fleet : newState.getAllFleets()) {
       // don't fire fleet created events
       if (oldState.getFleet(fleet.getId()) != null
         && fleet.getStarId() != null
         && oldState.starIsVisible(fleet.getStarId())) {
-        return Collections.singletonList(new FleetArrivedEvent(fleet.getId()));
+        events.add(new FleetArrivedEvent(fleet.getId()));
       }
     }
 
-    return Collections.emptyList();
+    return events;
   }
 
   @Override
