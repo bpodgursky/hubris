dfc66b636a4620e48396b9c22492adf8b11b7488
Chris Mullins <chris.mullins10@gmail.com>
2013-05-27 19:13:59 -0700

    handle destroyed fleets

--- src/main/java/com/bpodgursky/hubris/events/factories/FleetArrivedFactory.java
+++ src/main/java/com/bpodgursky/hubris/events/factories/FleetArrivedFactory.java
@@ -3,6 +3,7 @@ package com.bpodgursky.hubris.events.factories;
 import com.bpodgursky.hubris.events.FleetArrivedEvent;
 import com.bpodgursky.hubris.universe.Fleet;
 import com.bpodgursky.hubris.universe.GameState;
+import com.bpodgursky.hubris.universe.Star;
 import com.google.common.collect.Lists;
 
 import java.util.List;
@@ -18,7 +19,18 @@ public class FleetArrivedFactory implements EventFactory<FleetArrivedEvent> {
       if (oldState.getFleet(fleet.getId()) != null
         && fleet.getStarId() != null
         && oldState.starIsVisible(fleet.getStarId())) {
-        events.add(new FleetArrivedEvent(fleet.getId()));
+        events.add(new FleetArrivedEvent(fleet.getId(), fleet.getStarId()));
+      }
+    }
+
+    // Handle fleets that were destroyed
+    for (Fleet fleet : oldState.getAllFleets()) {
+      if (newState.getFleet(fleet.getId()) == null && !fleet.getDestinations().isEmpty()) {
+        int destinationStar = fleet.getDestinations().get(0);
+
+        if (oldState.starIsVisible(destinationStar) && newState.starIsVisible(destinationStar)) {
+          events.add(new FleetArrivedEvent(fleet.getId(), destinationStar));
+        }
       }
     }
 
