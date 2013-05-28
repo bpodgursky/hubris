8867d754eef0fe0cf22c3aae010ba0f8cc5b1b41
Chris Mullins <chris.mullins10@gmail.com>
2013-05-27 18:19:59 -0700

    fleet created event

--- src/main/java/com/bpodgursky/hubris/event/StateProcessor.java
+++ src/main/java/com/bpodgursky/hubris/event/StateProcessor.java
@@ -4,6 +4,8 @@ import com.bpodgursky.hubris.events.EventListener;
 import com.bpodgursky.hubris.events.factories.CashChangeFactory;
 import com.bpodgursky.hubris.events.factories.EventFactory;
 import com.bpodgursky.hubris.events.factories.FleetArrivedFactory;
+import com.bpodgursky.hubris.events.factories.FleetCreatedFactory;
+import com.bpodgursky.hubris.events.factories.FleetSpottedFactory;
 import com.bpodgursky.hubris.universe.GameState;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Lists;
@@ -15,8 +17,10 @@ import java.util.List;
 public class StateProcessor {
 
   public static final List<EventFactory> DEFAULT_FACTORIES = Lists.<EventFactory>newArrayList(
-      new CashChangeFactory(),
-      new FleetArrivedFactory()
+    new CashChangeFactory(),
+    new FleetArrivedFactory(),
+    new FleetSpottedFactory(),
+    new FleetCreatedFactory()
   );
 
   private final List<EventFactory> eventFactories = Lists.newArrayList(DEFAULT_FACTORIES);
